610ba869743ed6f8fd123f9b1f5462dcd130e903
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-04-26 01:38:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -10,8 +10,8 @@ function forLoop(arr){
 }
 
 function whileLoop(num){
-  let countdown = num
-  while (countdown > 0) {
-    console.log(--countdown)
+  while (num > 0) {
+    console.log(--num)
   }
+  return "done"
 }
